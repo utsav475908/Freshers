6865d3ed59cfbf4cb6c1c7da72ecf53c2c6d36b6
Harman <Harman@blrmccimac03s-imac.local>
2017-01-10 16:14:46 +0530

    Initial commit by a fresher

--- SingleViewTutorial/ViewController.m
+++ SingleViewTutorial/ViewController.m
@@ -8,20 +8,47 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
+@property(strong,nonatomic)IBOutlet UITableView *tableView;
+@property(strong,nonatomic)NSArray *amadeus;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.amadeus = @[@"this",@"that"];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+#pragma DataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.amadeus.count;
 }
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+   UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    cell.textLabel.text = self.amadeus[indexPath.row];
+    cell.detailTextLabel.text = @"MyAPP";
+    return cell;
+    
+}
+
+
+
+
+#pragma Delegate
+
+
+
 
 @end
